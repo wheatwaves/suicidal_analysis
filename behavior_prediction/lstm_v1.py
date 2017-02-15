9309dbdfdc0287b20aba477d839e65cf1b178f42
wangsen <wheatwang@sohu-inc.com>
2017-02-15 16:13:36 -0500

    a

--- behavior_prediction/lstm_v1.py
+++ behavior_prediction/lstm_v1.py
@@ -6,37 +6,43 @@ import cPickle
 SU = ['CC0001', 'CC0005', 'CC0006', 'CC0008', 'CC0011', 'CC0013']
 CO = ['CC0004', 'CC0020', 'CC0021', 'CC0022', 'CC0050', 'CC0051']
 MH = ['CC0002', 'CC0010', 'CC0028', 'CC0031', 'CC0035', 'CC0036']
-batch_size = 32
-DATA_DIR = '../data/svm_input/'
-histories = [5,10,15,20]
+DATA_DIR = '../data/svm_input_with_framerate_30/'
 feature_size = 14
-output_dims = [16,32,64,128]
-dropouts = [0.1,0.3,0.5]
-def load_data(train_file,validation_file,test_file,history):
+output_dims = [64]
+dropouts = [0.5]
+def load_data(train_file,validation_file,test_file):
 	train_X, train_Y, validation_X, validation_Y, test_X, test_Y = [],[],[],[],[],[]
 	for filename in train_file:
 		data = cPickle.load(open(DATA_DIR+filename))
+		file_X, file_Y = [],[]
 		for line in data:
-			train_X.append([line[-1-feature_size:-1]])
-			train_Y.append([line[-1:0]])
+			file_X.append(line[-1-feature_size:-1])
+			file_Y.append(line[-1:0])
+		train_X.append(file_X)
+		train_Y.append(file_Y)
 	for filename in validation_file:
 		data = cPickle.load(open(DATA_DIR+filename))
+		file_X, file_Y = [],[]
 		for line in data:
-			validation_X.append([line[-1-feature_size:-1]])
-			validation_Y.append([line[-1:0]])
+			file_X.append(line[-1-feature_size:-1])
+			file_Y.append([line[-1:0]])
+		validation_X.append(file_X)
+		validation_Y.append(file_Y)
 	for filename in test_file:
 		data = cPickle.load(open(DATA_DIR+filename))
+		file_X, file_Y = [],[]
 		for line in data:
-			test_X.append([line[-1-feature_size:-1]])
-			test_Y.append([line[-1:0]])
-
+			file_X.append(line[-1-feature_size:-1])
+			file_Y.append(line[-1:0])
+		test_X.append(file_X)
+		test_Y.append(file_Y)
 	return train_X, train_Y, validation_X, validation_Y, test_X, test_Y
 
 
 
-def train_lstm(train_X, train_Y, validation_X, validation_Y, output_dim, history, dropout):
+def train_lstm(train_X, train_Y, validation_X, validation_Y, output_dim, dropout):
 	model = Sequential()
-	model.add(LSTM(output_dim=output_dim, stateful = True, return_sequences = True, activation='sigmoid', inner_activation='hard_sigmoid', dropout_W=dropout, dropout_U=dropout, batch_input_shape = (1,1,feature_size)))
+	model.add(LSTM(output_dim=output_dim, return_sequences = True, activation='sigmoid', inner_activation='hard_sigmoid', dropout_W=dropout, dropout_U=dropout, input_shape = (len(train_X[0]),len(train_X[0][0])) ))
 	# model.add(Dropout(0.2))
 	model.add(Dense(1))
 	model.add(Activation('sigmoid'))
@@ -44,16 +50,15 @@ def train_lstm(train_X, train_Y, validation_X, validation_Y, output_dim, history
 	model.compile(loss='binary_crossentropy',
 	              optimizer=adam,
 	              metrics=['binary_accuracy','recall','precision'])
-
-	model.fit(train_X, train_Y, batch_size = batch_size, nb_epoch=10, validation_data=(validation_X, validation_Y))
+	
+	model.fit(train_X, train_Y, batch_size = len(train_X), nb_epoch=10, validation_data=(validation_X, validation_Y))
 	# score = model.evaluate(X_test, Y_test, batch_size = batch_size)
 
 def grid_search(train_file, validation_file, test_file):
-	for history in histories:
-		for output_dim in output_dims:
-			for dropout in dropouts:
-				train_X, train_Y, validation_X, validation_Y, test_X, test_Y = load_data(train_file,validation_file,test_file,history)
-				train_lstm(train_X, train_Y, validation_X, validation_Y, output_dim, history, dropout)
+	for output_dim in output_dims:
+		for dropout in dropouts:
+			train_X, train_Y, validation_X, validation_Y, test_X, test_Y = load_data(train_file,validation_file,test_file)
+			train_lstm(train_X, train_Y, validation_X, validation_Y, output_dim, history, dropout)
 if __name__ == '__main__':
 	train_file = SU[:4]+CO[:4]+MH[:4]
 	validation_file = SU[4:]+CO[4:]+MH[4:]
