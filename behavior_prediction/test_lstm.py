ce0ba14f4c9b054da626655222106b2e505899fe
wangsen <wheatwang@sohu-inc.com>
2017-02-15 14:31:06 -0500

    changes

--- behavior_prediction/test_lstm.py
+++ behavior_prediction/test_lstm.py
@@ -3,16 +3,16 @@ from keras.layers import Dense, Dropout, Activation
 from keras.layers import Embedding
 from keras.layers import LSTM
 import cPickle
-
-X=[[1,2,3],[4,5,6]]
-Y=[0,1]
+from keras.utils import np_utils
+X = [[[1,1],[1,1]],[[2,2],[2,2]]]
+Y=[[0,0],[1,1]]
+y = np_utils.to_categorical(Y)
 model = Sequential()
-model.add(LSTM(output_dim=2, return_sequences = True, activation='sigmoid', inner_activation='hard_sigmoid', input_shape = (1,3)))
-# model.add(Dropout(0.2))
+model.add(LSTM(output_dim=2, return_sequences = True, activation='sigmoid', inner_activation='hard_sigmoid', input_shape = (2,2)))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 model.compile(loss='binary_crossentropy',
               optimizer=adam,
               metrics=['binary_accuracy','recall','precision'])
-model.fit(X, Y, batch_size = 1, nb_epoch=10)
+model.fit(X, Y, batch_size = len(X), nb_epoch=10)
